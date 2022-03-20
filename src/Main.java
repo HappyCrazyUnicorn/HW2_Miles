6fdf563cb1ef5a1f2d6ac0dfa72f0cb7f6e06a13
HappyCrazyUnicorn <pankova.mary.v@gmail.com>
2022-03-20 14:35:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int ticketCost = 3750;
+        int mileCost = 20;
+        int miles;
+
+        miles = ticketCost / mileCost;
+
+        System.out.println("Количество бонусных миль за билет стоимостью " + ticketCost + " рублей составит " + miles);
+
+    }
+}
