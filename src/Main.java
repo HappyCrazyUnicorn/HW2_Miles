3557eaa3f408e375b4dbc700893f87e3e48297e1
HappyCrazyUnicorn <pankova.mary.v@gmail.com>
2022-03-20 14:47:42 +0300

    Add formating

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
         int ticketCost = 3750;
         int mileCost = 20;
         int miles;
 
         miles = ticketCost / mileCost;
 
-        System.out.println("Количество бонусных миль за билет стоимостью " + ticketCost + " рублей составит " + miles);
+        System.out.println("Количество бонусных миль за билет стоимостью " + ticketCost + " рублей составит:  " + miles);
 
     }
 }
